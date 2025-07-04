ae16fd974c5a9588c58fd9c71ebbfb7c837c2072
james-rod <Jamesbrod21@gmail.com>
2025-07-04 14:26:08 -0400

    added the routes to the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AddAnimePage from "./pages/AddAnimePage";
 import Navbar from "./components/Navbar";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
     <Box minH={"100vh"}>
@@ -10,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/add" element={<AddAnimePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Box>
   );
